2fe1c0ff9370cbf0c757edd24bcaaebaf2b5d924
bilkn <65674342+bilkn@users.noreply.github.com>
2020-12-27 12:17:45 +0300

    feat: add general site components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,47 @@
-import logo from './logo.svg';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import './App.css';
 
-function App() {
+function App() {}
+
+function siteHead() {
+  return <header className="site-head"></header>;
+}
+
+function Main() {
+  <PersonList />;
+}
+
+function PersonList() {
+  <Person />;
+}
+
+function Person(props) {
+  const { personName, birthday, imgSrc } = props;
   return (
-      
+    <div className="person">
+      <div className="person-img-container">
+        <img
+          className="person-img-container__img"
+          src={imgSrc}
+          alt={personName}
+        />
+      </div>
+      <div className="person-info">
+        <p className="person-info__name">{personName}</p>
+        <p className="person-info__birthday">{birthday}</p>
+      </div>
+    </div>
+  );
+}
+
+function mobileNav() {
+  return (
+    <nav className="mobile-nav">
+      <button className="mobile-nav__btn">
+        <i class="fas fa-plus"></i>
+      </button>
+    </nav>
   );
 }
 
