f871c8d898046eb2abc8c72c65afa23efcb76f2f
bilkn <65674342+bilkn@users.noreply.github.com>
2020-12-31 19:16:16 +0300

    feat: add person ui for adding new people

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,17 @@
 import AppHead from "../AppHead/AppHead";
 import HomeMain from "../HomeMain/HomeMain";
 import MobileNav from "../MobileNav/MobileNav";
+import AddPersonUI from "../AddPersonUI/AddPersonUI";
+import {useState} from "react";
 
 function App() {
+  const [showAddPersonUI, setShowAddPersonUI] = useState(false);
   return (
     <>
       <AppHead />
       <HomeMain />
-      <MobileNav />
+      <MobileNav state = {showAddPersonUI} callback={setShowAddPersonUI} />
+      {showAddPersonUI && <AddPersonUI callback = {()=> setShowAddPersonUI(!showAddPersonUI)} />}
     </>
   );
 }
--- src/components/MobileNav/MobileNav.js
+++ src/components/MobileNav/MobileNav.js
@@ -1,8 +1,8 @@
-import "./MobileNav.scss";
-function MobileNav() {
+import './MobileNav.scss';
+function MobileNav({ state, callback }) {
   return (
     <nav className="mobile-nav">
-      <button className="mobile-nav__add-btn">
+      <button className="mobile-nav__add-btn" onClick={() => callback(!state)}>
         <i className="fas fa-plus"></i>
       </button>
     </nav>
