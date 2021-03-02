19b07576b203076986c1bc2df01270c95b2b6113
bilkn <bilkan.konus@hotmail.com>
2021-03-02 15:29:30 +0300

    fix: update demoData

--- src/demoData.js
+++ src/demoData.js
@@ -2,33 +2,33 @@ const demoData = {
   people: [
     {
       id: 1,
-      name: 'Michael John',
-      birthday: '20.10.1985',
-      picture: './assets/man-1.png',
-    },
-    {
-      id: 2,
-      name: 'Niho Kerr',
-      birthday: '06.04.1993',
-      picture: './assets/girl-1.png',
-    },
-    {
-      id: 3,
-      name: 'Tom Bloom',
-      birthday: '12.02.1990',
-      picture: './assets/man-2.png',
-    },
-    {
-      id: 4,
       name: 'Derek Green',
       birthday: '20.05.1992',
       picture: './assets/man-3.png',
     },
     {
-      id: 5,
+      id: 2,
       name: 'Lewis Milton',
       birthday: '15.11.1980',
       picture: './assets/man-4.png',
+    },
+    {
+      id: 3,
+      name: 'Michael John',
+      birthday: '20.10.1985',
+      picture: './assets/man-1.png',
+    },
+    {
+      id: 4,
+      name: 'Niho Kerr',
+      birthday: '06.04.1993',
+      picture: './assets/girl-1.png',
+    },
+    {
+      id: 5,
+      name: 'Tom Bloom',
+      birthday: '12.02.1990',
+      picture: './assets/man-2.png',
     },
   ],
 };
